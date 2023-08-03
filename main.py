ed99a43e0bd589d206af6773be9a155fdbf9fa3b
Tony Aiello <heytonyyy3@gmail.com>
2023-08-03 12:24:51 -0700

    init commit -- pokemon data scraped

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+url = "https://pokemondb.net/pokedex/all"
+
+# Get HTML from URL
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Get table from HTML
+table = soup.find("table", class_="data-table")
+
+# Get column headers
+headers = [header.text.strip() for header in table.find("tr").find_all("th")]
+
+# Get data from each row
+rows = []
+for row in table.find_all("tr")[1:]:
+    row_data = [cell.text.strip() for cell in row.find_all("td")]
+    row_dict = dict(zip(headers, row_data))
+    rows.append(row_dict)
+
+
+# Open file for writing each column to a separate text file
+
+# Get column headers
+headers = list(rows[0].keys())
+
+# Open file for writing each column
+for header in headers:
+    with open(f"{header}.txt", "w", newline="") as f:
+        writer = csv.writer(f)
+
+        # Write header row
+        writer.writerow([header])
+
+        # Write column data from each row
+        for row in rows:
+            writer.writerow([row[header]])
