331d752c1c470439238585d6645df859175cc60f
Tony Aiello <heytonyyy3@gmail.com>
2023-08-03 12:32:55 -0700

    update with URLS

--- main.py
+++ main.py
@@ -38,3 +38,22 @@ for header in headers:
         # Write column data from each row
         for row in rows:
             writer.writerow([row[header]])
+
+
+# Get image URLs
+img_urls = []
+
+for row in table.find_all("tr")[1:]:
+    # Get img src attribute
+    img_url = (
+        row.find("td", class_="cell-num cell-fixed")
+        .find("span", class_="infocard-cell-img")
+        .img["src"]
+    )
+
+    # Append to list
+    img_urls.append(img_url)
+
+with open("image_urls.txt", "w") as f:
+    for url in img_urls:
+        f.write(url + "\n")
